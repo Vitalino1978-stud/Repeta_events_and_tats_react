ef86b46919e5edb2de4ca2c1acac670517222232
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-04-20 23:08:37 +0300

    Updates

---
+++ static/js/main.abcd79d8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonprepeta_events_and_tats_react=this.webpackJsonprepeta_events_and_tats_react||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var a=n(1),s=n.n(a),r=n(3),c=n.n(r),p=n(0);var d=function(){return Object(p.jsx)("div",{className:"App"})};c.a.render(Object(p.jsx)(s.a.StrictMode,{children:Object(p.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.abcd79d8.chunk.js.map
